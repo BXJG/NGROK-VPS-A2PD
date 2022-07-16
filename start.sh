7cb4be0f4dddfacde81039e616fe2298bf236da6
QiTianzu <50047003+QiTianzu@users.noreply.github.com>
2022-07-16 15:51:15 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/QiTianzu/NGROK-VPS-A2PD/master/ssh.sh | bash
+curl -fsSL https://raw.githubusercontent.com/QiTianzu/NGROK-VPS-A2PD/master/ngrok.sh | bash
