5d17036d59ca2a677d4f58f806740b6f353dce5b
QiTianzu <50047003+QiTianzu@users.noreply.github.com>
2022-07-16 17:11:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/QiTianzu/NGROK-VPS-A2PD/master/ssh.sh | bash
-curl -fsSL https://raw.githubusercontent.com/QiTianzu/NGROK-VPS-A2PD/master/ngrok.sh | bash
+curl -fsSL https://raw.githubusercontent.com/BXJG/NGROK-VPS-A2PD/master/ssh.sh | bash
+curl -fsSL https://raw.githubusercontent.com/BXJG/NGROK-VPS-A2PD/master/ngrok.sh | bash
