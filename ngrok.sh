98155bb229551446a6a98d5b2f6ec51b040a8987
QiTianzu <50047003+QiTianzu@users.noreply.github.com>
2022-07-17 00:55:35 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -9,8 +9,8 @@ ngrok_ssh_region="us"
 ngrok_aria2_authtoken="26eVqJ81bHeGatCnVfUahlTtkbC_hYsYoe6hA3tnUuKbYGxx"
 ngrok_aria2_region="us"
 
-echo -e "authtoken: ${ngrok_ssh_authtoken}\nregion: ${ngrok_ssh_region}\c" > /app/ngrok/.ngrok_ssh.yml
-echo -e "authtoken: ${ngrok_aria2_authtoken}\nregion: ${ngrok_aria2_region}\c" > /app/ngrok/.ngrok_aria2.yml
+echo -e "authtoken: ${ngrok_ssh_authtoken}\nregion: ${ngrok_ssh_region}" > /app/ngrok/.ngrok_ssh.yml
+echo -e "authtoken: ${ngrok_aria2_authtoken}\nregion: ${ngrok_aria2_region}" > /app/ngrok/.ngrok_aria2.yml
 
 echo "Start ngrok services."
 /app/ngrok/ngrok tcp --config /app/ngrok/.ngrok_ssh.yml 22
